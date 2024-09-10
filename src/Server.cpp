47229a25c2589a196f5c4cd4942294c10a4c09dc
Poushan <poushan.shan@gmail.com>
2024-09-10 19:13:35 +0530

    Match Digits Support

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <string>
 
-bool match_pattern(const std::string& input_line, const std::string& pattern) {
+bool match_pattern(const std::string &input_line, const std::string &pattern) {
     if (pattern.length() == 1) {
         return input_line.find(pattern) != std::string::npos;
-    }
-    else {
+    } else if (pattern == "\\d") {
+        return input_line.find_first_not_of("0123456789") != std::string::npos;
+    } else {
         throw std::runtime_error("Unhandled pattern " + pattern);
     }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     // Flush after every std::cout / std::cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
@@ -35,14 +36,14 @@ int main(int argc, char* argv[]) {
     //
     std::string input_line;
     std::getline(std::cin, input_line);
-    
+
     try {
         if (match_pattern(input_line, pattern)) {
             return 0;
         } else {
             return 1;
         }
-    } catch (const std::runtime_error& e) {
+    } catch (const std::runtime_error &e) {
         std::cerr << e.what() << std::endl;
         return 1;
     }
